305201cc1d420bc1a82e644a26b0b1e6975b7e9f
aashish <hiaashish83@gmail.com>
2024-07-18 22:40:49 +0530

    practice type conversion

---
+++ 01.js
@@ -0,0 +1,19 @@
+// console.log("aashish kumar")
+// console.log(typeof "afasdfl");
+// console.log(typeof b);
+// var ,let ,const
+
+let a1= "3336afsd"
+let b1= Number(a1);
+console.log(typeof b1);
+console.log(b1)
+
+let a= null
+let b= Number(a);
+console.log(typeof b);
+console.log(b)    //0
+
+let c= undefined
+let d= Number(c);
+console.log(typeof d);
+console.log(d)   //NaN
